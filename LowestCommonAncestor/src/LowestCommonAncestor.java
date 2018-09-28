7d68be2fce136c5ae502b0614cff77b0c6c9190e
healys10 <healys10@tcd.ie>
2018-09-28 13:19:24 +0100

    .

--- LowestCommonAncestor/src/LowestCommonAncestor.java
+++ LowestCommonAncestor/src/LowestCommonAncestor.java
@@ -1,5 +1,46 @@
+import java.util.ArrayList; 
+import java.util.List; 
+  
+class Node { 
+    int data; 
+    Node left, right; 
+  
+    Node(int value) { 
+        data = value; 
+        left = right = null; 
+    } 
+} 
+  
+public class LowestCommonAncestor 
+{ 
+  
+    Node root; 
+    private List<Integer> path1 = new ArrayList<>(); 
+    private List<Integer> path2 = new ArrayList<>(); 
+  
+    int findLCA(int n1, int n2) { 
+        path1.clear(); 
+        path2.clear(); 
+        return findLCAInternal(root, n1, n2); 
+    } 
+  
+    private int findLCAInternal(Node root, int n1, int n2) { 
+  
+        if (!findPath(root, n1, path1) || !findPath(root, n2, path2)) { 
+            System.out.println((path1.size() > 0) ? "n1 is present" : "n1 is missing"); 
+            System.out.println((path2.size() > 0) ? "n2 is present" : "n2 is missing"); 
+            return -1; 
+        } 
+  
+        int i; 
+        for (i = 0; i < path1.size() && i < path2.size(); i++) { 
+              
+            if (!path1.get(i).equals(path2.get(i))) 
+                break; 
+        } 
+  
+        return path1.get(i-1); 
+    } 
+}     
+  
 
-public class LowestCommonAncestor
-{
-
-}
