8de27379d2580c886f62af49682533685555f3ce
healys10 <healys10@tcd.ie>
2018-09-28 14:18:00 +0100

    ..

--- LowestCommonAncestor/src/LowestCommonAncestor.java
+++ LowestCommonAncestor/src/LowestCommonAncestor.java
@@ -41,6 +41,36 @@ public class LowestCommonAncestor
   
         return path1.get(i-1); 
     } 
+    private boolean findPath(Node root, int n, List<Integer> path) 
+    { 
+        // base case 
+        if (root == null) { 
+            return false; 
+        } 
+          
+        // Store this node . The node will be removed if 
+        // not in path from root to n. 
+        path.add(root.data); 
+  
+        if (root.data == n) { 
+            return true; 
+        } 
+  
+        if (root.left != null && findPath(root.left, n, path)) { 
+            return true; 
+        } 
+  
+        if (root.right != null && findPath(root.right, n, path)) { 
+            return true; 
+        } 
+  
+        // If not present in subtree rooted with root, remove root from 
+        // path[] and return false 
+        path.remove(path.size()-1); 
+  
+        return false; 
+    } 
+  
 }     
   
 
