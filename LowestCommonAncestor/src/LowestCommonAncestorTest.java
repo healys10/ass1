49fc0ee29896c7a61e2faaacb9fcb26d308321b3
healys10 <healys10@tcd.ie>
2018-09-29 16:10:27 +0100

    ..

--- LowestCommonAncestor/src/LowestCommonAncestorTest.java
+++ LowestCommonAncestor/src/LowestCommonAncestorTest.java
@@ -1,4 +1,5 @@
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -6,9 +7,20 @@ public class LowestCommonAncestorTest
 {
 
 	@Test
-	public void test()
+	public void testEmpty()
 	{
-		fail("Not yet implemented");
+		LowestCommonAncestor tree = new LowestCommonAncestor();
+		assertEquals("Checking empty tree", -1, tree.findLCA(2,3));
+	}
+	
+	@Test
+	public void testSingle()
+	{
+		LowestCommonAncestor tree = new LowestCommonAncestor();
+		tree.root = new Node(1);
+		assertEquals("Checking tree with single node at 1,1", 1, tree.findLCA(1,1));
+		assertEquals("Checking tree with single node at other", -1, tree.findLCA(2,6));
+		assertEquals("Checking tree with single node at other", -1, tree.findLCA(0,0));
 	}
 
 }
