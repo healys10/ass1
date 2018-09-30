b09bcb8cf72277c1359c547bd4df1878fd54b1a1
healys10 <healys10@tcd.ie>
2018-09-30 15:31:24 +0100

    ...

--- LowestCommonAncestor/src/LowestCommonAncestorTest.java
+++ LowestCommonAncestor/src/LowestCommonAncestorTest.java
@@ -22,5 +22,21 @@ public class LowestCommonAncestorTest
 		assertEquals("Checking tree with single node at other", -1, tree.findLCA(2,6));
 		assertEquals("Checking tree with single node at other", -1, tree.findLCA(0,0));
 	}
+	
+	@Test
+	public void testMultiple()
+	{
+		LowestCommonAncestor tree = new LowestCommonAncestor();
+		tree.root = new Node(1);
+		tree.root.left = new Node(2);
+		tree.root.right = new Node(3);
+		tree.root.left.left = new Node(4);
+		tree.root.left.right = new Node(5);
+		tree.root.right.right = new Node(6);
+		tree.root.right.right.right = new Node(7);
+		assertEquals("Checking tree with multiple nodes", 2, tree.findLCA(4,5));
+		assertEquals("Checking tree with multiple nodes", 1, tree.findLCA(3,5));
+		assertEquals("Checking tree with multiple nodes", 6, tree.findLCA(6,7));
+	}
 
 }
