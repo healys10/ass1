2e9348b85777470b6fe74897fe24e9acb6927305
healys10 <healys10@tcd.ie>
2018-10-01 15:35:43 +0100

    .

--- LowestCommonAncestor/src/LowestCommonAncestorTest.java
+++ LowestCommonAncestor/src/LowestCommonAncestorTest.java
@@ -58,8 +58,31 @@ public class LowestCommonAncestorTest
 		tree.root.right.left = new Node(7);
 		assertEquals("Checking non-present", -1, tree.findLCA(22,5));
 		assertEquals("Checking non-present", -1, tree.findLCA(18,115));
-		assertEquals("Checking non-present nodes", -1, tree.findLCA(0,7));
+		assertEquals("Checking non-present", -1, tree.findLCA(0,7));
 		
+	}
+	
+	@Test
+	public void testNegativeForPositive()
+	{
+		LowestCommonAncestor tree = new LowestCommonAncestor();
+		tree.root = new Node(1);
+		tree.root.left = new Node(2);
+		tree.root.left.left = new Node(3);
+		assertEquals("Checking negative", -1, tree.findLCA(-2,2));
+		assertEquals("Checking negative", -1, tree.findLCA(-1,-3));
+	}
+	
+	@Test
+	public void testNegative()
+	{
+		LowestCommonAncestor tree = new LowestCommonAncestor();
+		tree.root = new Node(-2);
+		tree.root.left = new Node(-5);
+		tree.root.left.left = new Node(-4);
+		tree.root.right = new Node(-1);
+		assertEquals("Checking", -2, tree.findLCA(-5,-1));
+		assertEquals("Checking", -5, tree.findLCA(-5,-4));
 	}
 
 }
