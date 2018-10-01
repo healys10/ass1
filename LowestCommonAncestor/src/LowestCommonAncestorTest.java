af50133a60553d56e7a20b82e262709f2e010690
healys10 <healys10@tcd.ie>
2018-10-01 15:18:42 +0100

    ..

--- LowestCommonAncestor/src/LowestCommonAncestorTest.java
+++ LowestCommonAncestor/src/LowestCommonAncestorTest.java
@@ -26,7 +26,6 @@ public class LowestCommonAncestorTest
 		LowestCommonAncestor tree = new LowestCommonAncestor();
 		tree.root = new Node(1);
 		assertEquals("Checking tree with single node at 1,1", 1, tree.findLCA(1,1));
-		assertEquals("Checking tree with single node at other", -1, tree.findLCA(2,6));
 		assertEquals("Checking tree with single node at other", -1, tree.findLCA(0,0));
 	}
 	
@@ -44,6 +43,23 @@ public class LowestCommonAncestorTest
 		assertEquals("Checking tree with multiple nodes", 2, tree.findLCA(4,5));
 		assertEquals("Checking tree with multiple nodes", 1, tree.findLCA(3,5));
 		assertEquals("Checking tree with multiple nodes", 6, tree.findLCA(6,7));
+	}
+	
+	@Test
+	public void testOthers()
+	{
+		LowestCommonAncestor tree = new LowestCommonAncestor();
+		tree.root = new Node(1);
+		tree.root.left = new Node(2);
+		tree.root.left.left = new Node(3);
+		tree.root.left.left.right = new Node(4);
+		tree.root.right = new Node(5);
+		tree.root.right.right = new Node(6);
+		tree.root.right.left = new Node(7);
+		assertEquals("Checking non-present", -1, tree.findLCA(22,5));
+		assertEquals("Checking non-present", -1, tree.findLCA(18,115));
+		assertEquals("Checking non-present nodes", -1, tree.findLCA(0,7));
+		
 	}
 
 }
