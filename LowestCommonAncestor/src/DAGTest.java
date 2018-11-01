d9e0a9ff6ff9ac692dbf36ec59ea4426acbeaebc
healys10 <healys10@tcd.ie>
2018-11-01 10:49:03 +0000

    testing

---
+++ LowestCommonAncestor/src/DAGTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DAGTest
+{
+
+	@Test
+	public void test()
+	{
+DAG graph = new DAG(10);
+		 
+		graph.addEdge(1, 2);
+		graph.addEdge(2, 4);
+		graph.addEdge(2, 5);
+		graph.addEdge(4, 6);
+		graph.addEdge(4, 7);
+		
+		assertEquals(1, graph.indegree(4));
+		assertEquals(2, graph.outdegree(4));
+		assertEquals(5, graph.E());
+		assertEquals(10, graph.V());
+		String adj = "[6, 7]";
+		assertEquals(adj, graph.adj(4).toString());
+	}
+
+}
