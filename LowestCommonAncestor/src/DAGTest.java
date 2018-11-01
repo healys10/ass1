da3f705fa1951cc448eef8f58bd2c8cd54c9da5a
healys10 <healys10@tcd.ie>
2018-11-01 16:34:09 +0000

    ..

--- LowestCommonAncestor/src/DAGTest.java
+++ LowestCommonAncestor/src/DAGTest.java
@@ -115,5 +115,55 @@ public class DAGTest
 		assertFalse(graph.hasCycle());
 	}
 	
+	@Test
+	public void testLCA()
+	{
+		DAG graph = new DAG(10);
+		
+		//        3   4    8
+		//   0  1       6    7
+		//        2   5
+		
+		graph.addEdge(0, 1);
+		graph.addEdge(1, 3);
+		graph.addEdge(1, 2);
+		graph.addEdge(3, 4);
+		graph.addEdge(2, 5);
+		graph.addEdge(5, 6);
+		graph.addEdge(4, 6);
+		graph.addEdge(6, 7);
+		graph.addEdge(4, 8);
+		graph.addEdge(8, 7);
+		
+		assertEquals(1, graph.findLCA(4, 5));
+		assertEquals(8, graph.findLCA(7, 8));
+		assertEquals(6, graph.findLCA(6, 7));
+	}
+	
+	@Test
+	public void testLCAforNoCommonAncestors()
+	{
+		DAG graph = new DAG(10);
+		graph.addEdge(0, 1);
+		graph.addEdge(0, 3);
+		graph.addEdge(1, 3);
+		graph.addEdge(3, 2);
+		graph.addEdge(2, 4);
+		graph.addEdge(1, 5);
+		graph.addEdge(2, 5);
+		
+		//Check if function works
+		assertEquals(0, graph.findLCA(2, 1));
+		assertEquals(3, graph.findLCA(2, 3));
+		assertEquals(2, graph.findLCA(4, 5));
+		
+		//Check for vertex that doesn't exist
+		assertEquals(-1, graph.findLCA(8, 2));
+		
+		//Check negative
+		assertEquals(-1, graph.findLCA(-2, 3));
+		assertEquals(-1, graph.findLCA(3, -2));
+		assertEquals(-1, graph.findLCA(-2, -3));
+	}
 
 }
